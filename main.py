60dc79f2ee2fd563ffd58f7248b93503501934b8
Adam Phelps <adamkygm@gmail.com>
2020-02-15 08:42:56 +0000

    Changed to ec2 resource.

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 import boto3
 from vpc import create_vpc
 tag = 'force10'
-ec2_c = boto3.client('ec2')
-vpc = create_vpc(ec2_c,'172.19.0.0/24',tag)
+ec2_r = boto3.resource('ec2')
+vpc = create_vpc(ec2_r,'172.19.0.0/24',tag)
