3a8966237e5544b24b7d3e45d759dbe9d6cc398c
Adam Phelps <adamkygm@gmail.com>
2020-02-15 17:38:03 -0800

    Moved modules to fit Python best practices.  Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Simple Linux Lab',
+    version='0.1',
+    packages=['simple_lab'],
+    license='MIT',
+    install_requires=[
+        'boto3',
+    ]
+)
