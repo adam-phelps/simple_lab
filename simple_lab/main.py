43bcec4f0e7fa87499926c0f6641552fcc51669b
Adam Phelps <adamkygm@gmail.com>
2020-02-15 17:47:31 -0800

    Added create SG function.

--- simple_lab/main.py
+++ simple_lab/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import boto3
-from vpc import create_vpc
+from vpc import create_vpc, create_sg
 tag = 'force10'
 ec2_r = boto3.resource('ec2')
+ec2_c = boto3.resource('ec2')
 vpc = create_vpc(ec2_r,'172.19.0.0/24',tag)
+sg = create_sg(ec2_c,vpc,tag)
