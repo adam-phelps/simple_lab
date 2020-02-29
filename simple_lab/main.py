b4d41bcab2d2c859f2c0b63730987aa04ca66dd8
Adam Phelps <adamkygm@gmail.com>
2020-02-29 22:15:29 +0000

    Allowed config file to be JSON.

--- simple_lab/main.py
+++ simple_lab/main.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python
 import boto3
+import json
 from network import create_subnet,create_route_table,create_vpc, create_security_group, authorize_security_group_ingress,create_internet_gateway
 from security import create_key_pair
 from compute import create_instances
 
-# TODO: Import these from a JSON file in the userspace
-lab_tag = 'force10'
-
-ports = {
-    'ssh':'22'
-    }
-networks = {
-    'vpc-lab':'172.19.0.0/16',
-    'public-sub-lab':'172.19.1.0/24'
-    }
-keys = {
-    'ssh-key':'key-pair-lab'
-    }
-instance_info = {
-    'ImageId':'ami-0a887e401f7654935',
-    'InstanceType':'t2.micro',
-    'KeyName':keys['ssh-key']
-}
+def read_config_file():
+    try:
+        with open('lab_config.json') as lab_config_file:
+            myconfig = json.load(lab_config_file)
+            return myconfig
+    except Exception as e:
+        print(f"{e}")
 
 if __name__ == "__main__":
+    myconfig = read_config_file()
+    lab_tag = myconfig['lab_tag']
     ec2_r = boto3.resource('ec2')
     ec2_c = boto3.client('ec2')
-    vpc = create_vpc(ec2_r, networks['vpc-lab'],lab_tag)
-    kp = create_key_pair(ec2_c,keys['ssh-key'])
+    vpc = create_vpc(ec2_r,myconfig['networks']['vpc-lab'],lab_tag)
+    kp = create_key_pair(ec2_c,myconfig['keys']['ssh-key'])
     sg = create_security_group(ec2_r,vpc,lab_tag)
-    authorize_security_group_ingress(ec2_c,sg,ports['ssh'])
+    authorize_security_group_ingress(ec2_c,sg,myconfig['ports']['ssh'])
     igw = create_internet_gateway(ec2_r,vpc)
     rt_table = create_route_table(vpc,igw)
-    sub = create_subnet(ec2_r,vpc,rt_table,networks['public-sub-lab'],lab_tag)
-    instance = create_instances(ec2_r,sg,sub,lab_tag,instance_info,'Public-Linux')
+    sub = create_subnet(ec2_r,vpc,rt_table,myconfig['networks']['public-sub-lab'],lab_tag)
+    instance = create_instances(ec2_r,sg,sub,lab_tag,myconfig['instance_info'],'Public-Linux')
     print(instance[0].public_ip_address)
