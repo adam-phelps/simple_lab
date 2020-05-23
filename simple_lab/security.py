1dcee1b79eff790b97f6bcc1928559f0ccf00970
Adam Phelps <adamkygm@gmail.com>
2020-05-23 14:40:19 -0400

    Resolved bug with key file not having correct permissions.

--- simple_lab/security.py
+++ simple_lab/security.py
@@ -1,8 +1,12 @@
+import os
+import stat
+
 def create_key_pair(ec2_c,my_key):
     try:
         response = ec2_c.create_key_pair(KeyName=my_key)
         with open(my_key + ".pem","w") as f:
             f.write(response['KeyMaterial'])
+        os.chmod(my_key + ".pem", stat.S_IRWXU)
         return response
     except:
         print("Key {} already exists.".format(my_key))
